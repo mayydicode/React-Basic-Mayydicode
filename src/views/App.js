5580387dc933784113d25a99a12564f2377b7a07
Mayy isme <3hung1998@gmail.com>
2023-11-21 23:28:47 +0700

    source day21

--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,12 @@
 import logo from "./logo.svg";
-import "./App.css";
+import "./App.scss";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello world with React</p>
+        <p>Hello world with React-Basic</p>
         <a
           className="App-link"
           href="https://reactjs.org"
