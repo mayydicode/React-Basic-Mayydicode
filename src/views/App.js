669fa65e411215a798f9ef8a7746f18a0dbad6a2
Mayy isme <3hung1998@gmail.com>
2023-12-21 22:36:18 +0700

    source day 21-12

--- src/views/App.js
+++ src/views/App.js
@@ -18,7 +18,7 @@ function App() {
 
       <ToastContainer
         position="top-right"
-        autoClose={3000}
+        autoClose={1500}
         hideProgressBar={false}
         newestOnTop={false}
         closeOnClick
